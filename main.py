f76c0b0868f3d3d2019f46b83713a019edc07f32
GPetilon <33421368+GPetilon@users.noreply.github.com>
2019-10-07 15:41:51 +0200

    Update main.py
    Le bot summon dès qu'il a assez de mana et met tout dans la tête de l'adversaire

--- main.py
+++ main.py
@@ -51,21 +51,21 @@ def creation_cards_lists (card_number, instance_id, location, card_type, cost, a
     card=Card(card_number, instance_id, location, card_type, cost, attack, defense, abilities)
     if card.get_location()==0 :
         carte_en_main.append(card)
-
+        
     elif card.get_location() == 1:
         carte_player_en_jeu.append(card)
-
+       
     else:
         carte_enemi_en_jeu.append(card)
         print("1",file=sys.stderr)
     return carte_en_main, carte_player_en_jeu, carte_enemi_en_jeu
-
+      
 while True:
     carte_a_choisir = []
     carte_en_main = []
     carte_player_en_jeu = []
     carte_enemi_en_jeu = []
-
+    
     for i in range(2):
         player_health, player_mana, player_deck, player_rune, player_draw = [int(j) for j in input().split()]
     opponent_hand, opponent_actions = [int(i) for i in input().split()]
@@ -84,30 +84,30 @@ while True:
         my_health_change = int(my_health_change)
         opponent_health_change = int(opponent_health_change)
         card_draw = int(card_draw)
-
+        
         if (draw_phase):
             carte_a_choisir.append(Card( card_number, instance_id, location, card_type, cost, attack, defense, abilities))
         else:
             carte_en_main,carte_player_en_jeu,carte_enemi_en_jeu = creation_cards_lists (card_number, instance_id, location, card_type, cost, attack, defense, abilities)
-
+            
     # Write an action using print
     # To debug: print("Debug messages...", file=sys.stderr)
-
+    
     if (draw_phase):
             print("PICK 1")
             count+=1
             if (count == 30):
                 draw_phase = False
-
+           
     else:
-
+        command =""
         for i in carte_en_main:
             if (player_mana>=i.get_cost()):
-                print ("SUMMON "+str(i.get_instance_id())+";")
+                command+="SUMMON "+str(i.get_instance_id())+";"
         print(carte_en_main, file=sys.stderr)
         for i in carte_player_en_jeu:
-            if (len(carte_enemi_en_jeu)!=0):
-                print("ATTACK "+str(i.get_instance_id())+" "+str(carte_enemi_en_jeu[0].get_instance_id())+";")
-            else:
-                print("ATTACK "+str(i.get_instance_id())+" -1"+";")
-        print ("PASS")
+            #if (len(carte_enemi_en_jeu)!=0):
+                #command+="ATTACK "+str(i.get_instance_id())+" "+str(carte_enemi_en_jeu[0].get_instance_id())+";"
+            #else: 
+                command+="ATTACK "+str(i.get_instance_id())+" -1"+";"
+        print (str(command)+"PASS")
